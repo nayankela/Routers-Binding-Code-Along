5e51e9be9f3b7aad838b68a3e5bdb193fcab89e6
nayankela <nayankela09@gmail.com>
2020-10-08 01:11:39 +0530

    done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DataBindingComponent } from './data-binding/data-binding.component'
+import { AboutusComponent } from './aboutus/aboutus.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component:HomeComponent},
+   {path: 'databinding', component:DataBindingComponent},
+   {path: 'aboutus', component:AboutusComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
